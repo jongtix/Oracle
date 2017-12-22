a59dfff958081d8390976649cd7cea71c1380c49
jongtix <jong1145@naver.com>
2017-12-22 17:47:02 +0900

    171222

---
+++ 쿼리문/171222.sql
@@ -0,0 +1,38 @@
+CREATE TABLE menu (
+    seq    NUMBER PRIMARY KEY,
+    name   VARCHAR2(50)
+);
+
+CREATE SEQUENCE menu_seq START WITH 1 INCREMENT BY 1 NOCYCLE;
+
+INSERT INTO menu VALUES (
+    menu_seq.NEXTVAL,
+    'Â¥Àå'
+);
+
+INSERT INTO menu VALUES (
+    menu_seq.NEXTVAL,
+    'Â«»Í'
+);
+
+INSERT INTO menu VALUES (
+    menu_seq.NEXTVAL,
+    'ººÀ½¹ä'
+);
+
+INSERT INTO menu VALUES (
+    menu_seq.NEXTVAL,
+    'ÀâÃ¤¹ä'
+);
+
+INSERT INTO menu VALUES (
+    menu_seq.NEXTVAL,
+    'ÅÁ¼öÀ°'
+);
+
+SELECT
+    *
+FROM
+    menu;
+    
+    commit;
