9b873306ad143370b62196e991435df754ba5d35
jongtix <jong1145@naver.com>
2018-01-18 17:43:28 +0900

    180118

---
+++ 쿼리문/180118.sql
@@ -0,0 +1,79 @@
+SELECT
+    *
+FROM
+    member2;
+
+SELECT
+    *
+FROM
+    cols
+WHERE
+    table_name = 'BUY';
+
+SELECT
+    'private '
+    || DECODE(data_type,'NUMBER','int','DATE','Date','VARCHAR2','String',data_type)
+    || ' '
+    || lower(column_name)
+    || ';'
+FROM
+    cols
+WHERE
+    table_name = 'BUY';
+
+SELECT
+    'private '
+    || DECODE(data_type,'NUMBER','int','DATE','Date','VARCHAR2','String',data_type)
+    || ' '
+    || lower(column_name)
+    || ';'
+FROM
+    cols
+WHERE
+    table_name = 'BANK';
+
+SELECT
+    *
+FROM
+    buy;
+
+DESC buy;
+
+SELECT
+    *
+FROM
+    bank
+ORDER BY
+    account;
+
+SELECT
+    *
+FROM
+    cart;
+
+DESC cart;
+
+CREATE SEQUENCE buy_seq INCREMENT BY 1 START WITH 1;
+
+COMMIT;
+
+SELECT
+    *
+FROM
+    book;
+
+SELECT
+    *
+FROM
+    buy
+WHERE
+    buyer = 'jongtix';
+
+DELETE buy;
+
+SELECT
+    book_count
+FROM
+    book
+WHERE
+    book_id = '9';
